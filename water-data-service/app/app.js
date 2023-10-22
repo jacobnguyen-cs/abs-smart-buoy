22e88b1a53f538a071ee39407399dfffcea6b36a
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-10-22 16:24:56 -0500

    Added Dockerfile and changes to other files.
    Dockerfile added. Removed environment variables. Moved files relevant to service into /app.

--- water-data-service/app/app.js
+++ water-data-service/app/app.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const Database = require("./database/database");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 5000 || 3000;
 const waterDataRoute = require("./routes/waterDataRouter");
 
 const db = new Database();
--- water-data-service/app/database/config.js
+++ water-data-service/app/database/config.js
@@ -1,9 +1,17 @@
-require("dotenv").config();
+// require("dotenv").config();
+
+// module.exports = {
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT
+// };
 
 module.exports = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT
+    user: "postgres",
+    password: "password",
+    database: "water_sensor",
+    host: "localhost",
+    port: 5432
 };
