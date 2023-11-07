570b4f58763cd262f0eb092465ef0e7f34223ec9
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-11-06 20:02:22 -0600

    Integration of Timestream Complete
    Integration of Timestream complete for the water temperature service.

--- water-temperature-service/app/controllers/waterTempController.js
+++ water-temperature-service/app/controllers/waterTempController.js
@@ -1,60 +1,14 @@
-// /**
-//  * Gets all water sensor data
-//  * @async
-//  * @function
-//  * @param {object} db - Database connection
-//  * @returns {Promise<Array<object>>} A promise that resolves to an array objects containing the water sensor data
-//  */
-// async function getAllWaterTemp(db) {
-//     const result = await db.runQuery("SELECT * FROM water_temp");
-//     return result;
-// }
-
-// /**
-//  * Gets the water sensor data specified by the id
-//  * @async
-//  * @function
-//  * @param {object} db - Database connection
-//  * @param {int} id - Water temperature id
-//  * @returns {Promise<object>} A promise object containing the water sensor data
-//  */
-// async function getWaterTemp(db, id) {
-//     const result = await db.runQuery(`SELECT * FROM water_temp WHERE id=${id}`);
-//     return result;
-// }
-
-
-// /**
-//  * Adds data to the water sensor
-//  * @async
-//  * @function
-//  * @param {object} db - Database connection
-//  * @param {int} id - ID for water temperature
-//  * @param {decimal} temp - Water temperature in decimal value
-//  * @returns {Promise<object>} A promise object containing the water sensor data
-//  */
-// async function addWaterTemp(db, id, temp) {
-//     const result = await db.runQuery(`INSERT INTO water_temp (id, temp) VALUES(${id}, ${temp}) RETURNING *`);
-//     return result;
-// }
-
-// /**
-//  * Deletes data for the water sensor
-//  * @async
-//  * @function
-//  * @param {object} db - Database connection
-//  * @param {int} id - Water temperature id
-//  * @returns {Promise<object>} A promise object containing the water sensor data
-//  */
-// async function deleteWaterTemp(db, id) {
-//     const result = await db.runQuery(`DELETE FROM water_temp WHERE id=${id} RETURNING *`);
-//     return result;
-// }
-
 const { TimestreamWriteClient, WriteRecordsCommand } = require("@aws-sdk/client-timestream-write")
 const writeClient = new TimestreamWriteClient({ region: "us-east-2" });
 
-// Add water temperature data point to time-series database
+/**
+ * Stores water temperature in the time series database
+ * @async
+ * @function
+ * @param {decimal} value 
+ * @param {Time} time 
+ * @returns {Promise<object>} A promise object containing the water temperature data
+ */
 async function storeWaterTemp(value, time) {
     const DATABASE_NAME = "WaterTempDB"
     const TABLE_NAME = "WaterTempTable"
@@ -78,9 +32,9 @@ async function storeWaterTemp(value, time) {
         Records: records
     };
     const command = new WriteRecordsCommand(params);
-    const data = await writeClient.send(command);
+    const result = await writeClient.send(command);
+    return result;
 }
-
 
 module.exports = {
     storeWaterTemp
