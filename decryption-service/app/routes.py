c437618e23aa646f1916c60ddddb42af2802e6cd
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-11-19 15:58:34 -0600

    Implemented a service for decryption
    Testing communication between containers

--- decryption-service/app/routes.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flask import request, jsonify
-
-@app.route('/decrypt', methods=['POST'])
-def decryption_service():
-    if not request.json:
-        return jsonify({'error': 'No data'}), 400
-    
-    return request.json
--- decryption-service/run.py
+++ decryption-service/run.py
@@ -1,4 +1,15 @@
-from app import app
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/decrypt', methods=['POST'])
+def decryption_service():
+    if not request.json:
+        return jsonify({'error': 'No data'}), 400
+    
+    return request.json
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
