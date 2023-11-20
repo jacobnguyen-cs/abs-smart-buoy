b8557ea1ae75ef71d9fb6fca46063aab45d69538
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-11-20 10:47:14 -0600

    Changed port
    Changed port from 80 to 5001

--- air-temperature-service/app/app.js
+++ air-temperature-service/app/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 80;
+const port = 5001;
 const airTempRoute = require("./routes/airTempRouter");
 
 app.use(express.json());
