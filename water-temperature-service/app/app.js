cfb8ad30d7580ea1a7b60ff98ae51f86ddf7a792
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-11-20 13:51:58 -0600

    Minor changes

--- water-temperature-service/app/app.js
+++ water-temperature-service/app/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000 || 3000;
+const port = 5000;
 const waterTempRoute = require("./routes/waterTempRouter");
 
 app.use(express.json());
