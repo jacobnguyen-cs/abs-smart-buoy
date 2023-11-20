3414b02bbb7e8bff08b0e126478e96849f8de71d
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-11-20 09:49:59 -0600

    Able to parse data for from-mobile messages that we send
    This code is able to take the bytes received from the satcomm-service and decode them to readable messages from the from-mobile messages.

--- decryption-service/run.py
+++ decryption-service/run.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 import requests
+import base64
 
 app = Flask(__name__)
 CORS(app)
@@ -11,10 +12,15 @@ def decryption_service():
     if not request.json:
         return jsonify({'error': 'No data'}), 400
     
-    url = 'http://water-temperature-service-alb-1534773001.us-east-2.elb.amazonaws.com/waterTemp/add'
-    r = requests.post(url, json=request.json)
-    
-    return str(r)
+    data = ''
+    if request.json['RawPayload'] and not request.json['Payload']:
+        data = base64.b64decode(request.json['RawPayload'])
+        data = data.decode('latin-1')
+
+    # url = 'http://water-temperature-service-alb-1534773001.us-east-2.elb.amazonaws.com/waterTemp/add'
+    # r = requests.post(url, json=jsonify(data))
+
+    return data
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000)
