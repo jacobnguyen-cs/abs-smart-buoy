095f4ed85f4543e3fa707d6f9fb122374bcfdab2
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-11-20 13:47:11 -0600

    Changed port
    Changed port from 80 to 5002

--- humidity-service/app/app.js
+++ humidity-service/app/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 80;
+const port = 5002;
 const humidityRoute = require("./routes/humidityRouter");
 
 app.use(express.json());
