0ab86d925e51302b5d5e2bf769d281e994022464
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-11-19 18:55:17 -0600

    Able to communicate to the water temperature service on AWS
    Was able to establish communication between the water temperature service hosted on AWS and is able to store data.

--- decryption-service/run.py
+++ decryption-service/run.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+import requests
+import time
+
 app = Flask(__name__)
 CORS(app)
 
@@ -9,7 +12,12 @@ def decryption_service():
     if not request.json:
         return jsonify({'error': 'No data'}), 400
     
-    return request.json
+    url = 'http://water-temperature-service-alb-1534773001.us-east-2.elb.amazonaws.com/waterTemp/add'
+    r = requests.post(url, json=request.json)
+    
+    return str(r)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    current_time = str(int(round(time.time() * 1000)))
+    print(current_time)
+    app.run(host='0.0.0.0', port=5001)
