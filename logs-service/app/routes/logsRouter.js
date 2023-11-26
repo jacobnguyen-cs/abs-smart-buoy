e24653b3b97158c99b2105ae9b6c0630ecc8f2c2
Caleb Williamson <caleb7981@tamu.edu>
2023-11-26 15:29:01 -0600

    Created logs microservice

---
+++ logs-service/app/routes/logsRouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const logsController = require("../controllers/logsController");
+
+router.post("/add", async (req, res) => {
+    try {
+        const log = req.body.log;
+        const time = req.body.time;
+        var result = await logsController.storeLog(log, time);
+        // result = {"type": req.body.type, "data": req.body.data};
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
+
+module.exports = router;
